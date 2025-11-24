a9b3124b2d43a47b493b6c9c22f0f6dd52da92e7
surajmore <155713352+suraj-pf@users.noreply.github.com>
2025-11-24 17:55:47 +0000

    created the create notes functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const fs = require('fs');
+const { log } = require('console');
 
 app.use(express.json());
 app.set('view engine','ejs')
@@ -9,9 +10,27 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    fs.readdir('./files',(err,files)=>{
+        res.render('index',{files:files});
+        });
 });
 
+
+app.post('/create',(req,res)=>{
+    console.log(req.body);
+    fs.writeFile(`./files/${req.body.title.split(" ").join("_")}.txt`, req.body.description, (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        res.redirect('/');
+    });
+});
+
+
+app.get('/files/:filename',(req,res)=>{
+    console.log('');
+});
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');

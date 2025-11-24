0dc1bbb75dda162d6e7d03db18287129ab6f8f80
surajmore <155713352+suraj-pf@users.noreply.github.com>
2025-11-24 20:44:40 +0000

    might add func later

--- index.js
+++ index.js
@@ -29,7 +29,35 @@ app.post('/create',(req,res)=>{
 
 
 app.get('/files/:filename',(req,res)=>{
-    console.log('');
+    fs.readFile(`./files/${req.params.filename}`,'utf8',(err,data)=>{
+        if(err){
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        res.render('show',{filename:req.params.filename, content:data});
+    });
+});
+
+app.get('/edit/:filename',(req,res)=>{
+    fs.readFile(`./files/${req.params.filename}`,'utf8',(err,data)=>{
+        if(err){
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        res.render('edit',{filename:req.params.filename, content:data});
+    });
+});
+
+app.post('/edit/',(req,res)=>{
+    fs.rename(`./files/${req.body.previous}.txt`, `./files/${req.body.newtitle}.txt`, (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        res.redirect('/');
+    });
 });
 
 app.listen(3000,()=>{

293c7c6c221cd423ede291fc2f3a843fb42bf88e
Suraj More <more.suraj.pf@gmail.com>
2025-11-28 16:35:41 +0530

    moving to authorization

--- 02_learning/app.js
+++ 02_learning/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
+const userModel = require('./models/user')
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-const userModel = require('./models/user')
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -16,18 +17,40 @@ app.get('/', (req, res) => {
 });
 
 app.post('/create', async (req, res) => {
-    let name,email,imageurl = req.body;
+    let { name, email, imageurl } = req.body;
     let user = await userModel.create({
-        name, // name : name
-        email : email,
-        imageurl : imageurl
-    })
-    res.send(user)
+        name: name,
+        email: email,
+        image: imageurl
+    });
+    res.redirect('/read');
 });
 
-app.get('/read', (req, res) => {
-    res.render('read');
+app.get('/read', async (req, res) => {
+    const users = await userModel.find();
+    res.render('read', { users });
 });
+
+app.get('/edit/:userid', async (req, res) => {
+    let user = await userModel.findOne({_id:req.params.userid})
+    res.render('edit', { user });
+});
+
+app.post('/edit/:userid', async (req, res) => {
+    const { name, email, imageurl } = req.body;
+    await userModel.findByIdAndUpdate(req.params.userid, {
+        name: name,
+        email: email,
+        image: imageurl
+    }, { new: true });
+    res.redirect('/read');
+});
+
+app.get('/delete/:userid', async (req, res) => {
+    await userModel.findByIdAndDelete(req.params.userid);
+    res.redirect('/read');
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
--- 02_learning/models/user.js
+++ 02_learning/models/user.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
+// Connect to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/testapp1')
-  .then(() => console.log("Database connected"))
-  .catch(err => console.log(err));
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
+// Define schema
 const UserSchema = new mongoose.Schema({
     image: String,
     name: String,
     email: String
 });
 
+// Export model
 module.exports = mongoose.model('User', UserSchema);
+
